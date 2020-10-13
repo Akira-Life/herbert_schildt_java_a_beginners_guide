46bb18bb90598eca639e689e58341152bac6ab2f
akira <mr.akira.life.ver98@gmail.com>
2020-10-13 03:24:22 +0300

    Commit 3

--- src/Practice/Practice_0_3_Data_Types_and_Operators.java
+++ src/Practice/Practice_0_3_Data_Types_and_Operators.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 /*
         Нужно выявить все простые числа от 2 до N-ое
 
+        ЗАДАЧА ОКАЗАЛОСЬ НЕ ПРОСТОЙ ДЛЯ МЕНЯ!
+        ЖЕЛАТЕЛЬНО ПЕРЕРЕШАТЬ ДРУГИМИ СПОСОБАМИ ЧЕРЕЗ ПАРУ НЕДЕЛЬ!
+
  */
 public class Practice_0_3_Data_Types_and_Operators {
     public static void main(String[] args) {
         boolean simpleNumbersStartup;
         int integers;
         int primeNumbers;
-
         for (integers = 2; integers < 100; integers++) {
             simpleNumbersStartup = true;
-
-
             for (primeNumbers = 2; primeNumbers <= integers / primeNumbers; primeNumbers++) {
                 if ((integers % primeNumbers) == 0) {
                     simpleNumbersStartup = false;
